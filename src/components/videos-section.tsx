231dc7247cd82e94892b71cbe47d75034d308490
associaasppibra <associaasppibra@gmail.com>
2025-11-03 21:16:23 +0000

    Update videos section

--- src/components/videos-section.tsx
+++ src/components/videos-section.tsx
@@ -33,7 +33,7 @@ export const VideosSection = () => {
                       <svg className="h-12 w-12 text-white" fill="currentColor" viewBox="0 0 20 20"><path d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"></path></svg>
                     </div>
                   </div>
-                  <div className="p-4">
+                  <div className="p-4 h-24">
                     <h3 className="font-semibold text-sm">{video.title}</h3>
                   </div>
                 </Link>
